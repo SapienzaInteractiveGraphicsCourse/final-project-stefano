93718cecb024ad454790af8768c7106896efe96b
Stefano <servillostefano@gmail.com>
2022-07-26 17:41:41 +0200

    First pages

--- src/main.js
+++ src/main.js
@@ -1,36 +1,76 @@
 import * as THREE from '../build/three.module.js';
 import {GLTFLoader} from '../loaders/GLTFLoader.js';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(45 , window.innerWidth/window.innerHeight, 0.25, 1000 );
-camera.position.set(5,5,5);
-camera.lookAt(new THREE.Vector3(0,2,0));
+var score_div = document.getElementById("title_score");
+var money_div = document.getElementById("money_score");
 
-scene.background = new THREE.Color(0xe0e0e0);
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const Light = new THREE.DirectionalLight(0xffffff);
-Light.position.set(0,20,10);
-scene.add(Light);
+var score = 0;
+var money = 0;
 
 const loader = new GLTFLoader();
-loader.load('models/RobotExpressive.glb', function(gltf) {
-	var model = gltf.scene;
-	model.position.x=-1;
-	model.position.y=-1;
-	model.position.z=1;
-	model.scale.x=model.scale.y=model.scale.z=0.3;
-	model.rotation.y=3.5;
-	scene.add(model);
 
-}, undefined, function(e) {
-	console.error(e);
-});
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(50 , window.innerWidth/window.innerHeight, 0.25, 1000 );
+var renderer = new THREE.WebGLRenderer();
+const background_loader = new THREE.TextureLoader()
+const Light = new THREE.DirectionalLight(0xffffff);
 
-camera.position.z = 5;
+buttons()
+
+window.onload = Scene();
+
+function Scene() {
+	camera.position.set(5,5,5);
+	camera.lookAt(new THREE.Vector3(0,2,0));
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	background_loader.load('images/background.jpg', function(texture){
+		scene.background = texture;
+	});
+
+	Light.position.set(0,20,10);
+	scene.add(Light);
+
+	document.getElementById("score").style.display = 'none';
+	document.getElementById("finish").style.display = 'none';
+	document.getElementById("rules").style.display = 'none';
+
+	camera.position.z = 5;
+}
+
+
+function buttons() {
+	document.getElementById("home_page_rules").onclick = () => {
+		document.getElementById("home_page").style.display = 'none';
+		document.getElementById("rules").style.display = 'grid';
+	}
+
+	document.getElementById("rules_home_page").onclick = () => {
+		document.getElementById("home_page").style.display = 'grid';
+		document.getElementById("rules").style.display = 'none';
+	}
+
+	document.getElementById("home_page_start").onclick = () => {
+		document.getElementById("home_page").style.display = 'none';
+		document.getElementById("score").style.display = 'grid';
+		score_div.innerText = 'SCORE: ' + score;
+		money_div.innerText = 'MONEY: ' + money;
+		loader.load('models/RobotExpressive.glb', function(gltf) {
+			var model = gltf.scene;
+			model.position.x=-1;
+			model.position.y=-1;
+			model.position.z=1;
+			model.scale.x=model.scale.y=model.scale.z=0.3;
+			model.rotation.y=3.5;
+			scene.add(model);
+		
+		}, undefined, function(e) {
+			console.error(e);
+		});
+	}
+}
 
 var animate = function () {
 	requestAnimationFrame( animate );
